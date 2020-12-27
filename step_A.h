bef298f5e6b546d02af17f03ef19ae9b73068629
Kryzar <antoine@hugounet.com>
2020-12-27 23:00:46 +0100

    feat(qacp): begin writting qacp

--- step_A.h
+++ step_A.h
@@ -7,13 +7,13 @@
 
 
 struct Params {
-	mpz_t	 N;				  // Integer to factor
-	unsigned k;				  // Integer k used in sqrt(kN)
-	mpz_t    n_lim;			  // Size of the cont. frac. expension of sqrt(kN)
-	unsigned n_want_factQns;  // Number of wanted factored Qns
+	mpz_t	 N;				   // Integer to factor
+	unsigned k;				   // Integer k used in sqrt(kN)
+	mpz_t    n_lim;			   // Size of the cont. frac. expension of sqrt(kN)
+	unsigned n_want_fact_Qns;  // Number of wanted factored Qns
 };
 
 typedef struct Params Params
-// @Funk Voir https://c.programmingpedia.net/fr/tutorial/2681/typedef
+// @Margot Voir https://c.programmingpedia.net/fr/tutorial/2681/typedef
 
 #endif
--- step_B.c
+++ step_B.c
@@ -7,12 +7,14 @@
 #include <step_A.h>
 #include <step_B.h>
 
+#define LIM (
 
-void create_AQ_pairs(const Params P, mpz_t* Ans, mpz_t* Qns, unsigned f,
+
+void create_AQ_pairs(const Params P, mpz_t* Ans, mpz_t* Qns, unsigned* f,
 					 mpz_t* exp_vects, mpz_t* hist_vects,
 					 const mpz_t* factor_base, const unsigned B) {
 /*
-	TODO: docstring me
+	TODO: Document me
 
 	:param P: Set of parameters for the problem (see step_A.h).
 	:param f: Numb. of factored Qns.
@@ -32,9 +34,32 @@ void create_AQ_pairs(const Params P, mpz_t* Ans, mpz_t* Qns, unsigned f,
 	// taille B.
 	unsigned* reduced_fb_indexes;  // prime_table
 	unsigned s_reduced_fb_indexes;  // s_prime_table
+	unsigned n;
 
 	Qn_odd_pows = (unsigned*)malloc(B * sizeof(unsigned));
 	reduced_fb_indexes = (unsigned*)malloc(B * sizeof(unsigned));
+	n = 0;
+
+	// TODO: Expand sqrt(kN), step 2) chez Margot
+	// TODO: Limite dynamique, step 3) chez Margot
+	
+	while (n < P.n_lim
+			&& &f < P.n_want_fact_Qns != 0 ? P.n_want_fact_Qns : dyn_lim
+			&& q_last != 1) {
+		// TODO: Définir dyn_lim (changer de nom ?) et q_last
+
+		// TODO: Calculer An-1 et Qn, step a) chez Margot
+		// On les note Anm1 et Qn dans la suite du programme
+
+		Ans[f] = Anm1;
+		Qns[f] = Qn;
+
+		// TODO: Appeler les add_*
+
+		&f ++;
+		n ++;
+	}
+			
 
 	/*******
 	* Free *
