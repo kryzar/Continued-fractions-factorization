bfd8947c3060643c934e291ea20f7694b49c0bb7
Skylitzes <margot.funk@free.fr>
2020-12-30 14:54:30 +0100

    change struct Params + mpz_array functions

--- src/step_A.c
+++ src/step_A.c
@@ -2,4 +2,25 @@
 	step_A.c
 */
 
-#include <step_A.h>
+#include "step_A.h"
+
+mpz_t * mpz_alloc_array(size_t s_array){
+      return (mpz_t *)malloc(s_array*sizeof(mpz_t)); 
+}
+
+void mpz_init_array(mpz_t *array, size_t s_array){
+      size_t i; 
+
+      for (i = 0; i < s_array; i++){
+            mpz_init(array[i]);
+      }
+}
+
+void mpz_free_array(mpz_t *array, size_t s_array){
+      size_t i; 
+      for (i = 0; i < s_array; i++){
+            mpz_clear(array[i]);
+      }
+      free(array); 
+      array = NULL;
+}
--- src/step_A.h
+++ src/step_A.h
@@ -5,15 +5,24 @@
 #ifndef STEP_A
 #define STEP_A
 
+#include <gmp.h>
+#include <stdlib.h>
 
 struct Params {
-	mpz_t	 N;				   // Integer to factor
-	unsigned k;				   // Integer k used in sqrt(kN)
-	mpz_t    n_lim;			   // Size of the cont. frac. expension of sqrt(kN)
-	unsigned n_want_fact_Qns;  // Number of wanted factored Qns
+	mpz_t	 N;			// Integer to factor
+	unsigned k;	            // Integer k used in sqrt(kN)
+	mpz_t    n_lim;		// Size of the cont. frac. expansion of sqrt(kN)
+	size_t nb_want_AQp;     // Number of wanted A-Q pairs (A_{n-1}, Qn) with
+                              // Qn factorisable with the primes of the factor base
 };
 
-typedef struct Params Params
-// @Margot Voir https://c.programmingpedia.net/fr/tutorial/2681/typedef
+// je sais pas pour le type de n_lim 
+
+typedef struct Params Params;
+
+
+mpz_t * mpz_alloc_array(size_t s_array);
+void mpz_init_array(mpz_t *array, size_t s_array);
+void mpz_free_array(mpz_t *array, size_t s_array);
 
 #endif
